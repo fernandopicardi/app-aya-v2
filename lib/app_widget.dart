2541678d20702618e5ac3bce74ea34268b06dae5
fernando-picardi <fernando@yangflow.us>
2025-05-31 17:22:40 -0300

    Estrutura base

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,1 +1,15 @@
 // App Widget - Root widget of the MaterialApp
+
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'App Aya',
+      home: Scaffold(body: Center(child: Text('App Aya'))),
+    );
+  }
+}
